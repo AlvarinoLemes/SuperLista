55f83693eaf6b93363e008877584d097bfac872b
AlvarinoLemes <alvarinoguilherme@hotmail.com>
2023-11-06 22:11:23 -0300

    antes de instalar os cors

--- superlista-backend/src/routes.js
+++ superlista-backend/src/routes.js
@@ -3,15 +3,18 @@ const routes = express.Router();
 
 const ListaController = require('./controllers/ListaController');
 const PriorityController = require('./controllers/PriorityController');
+const ContentController = require('./controllers/ContentController');
 
 // Rota SuperLista
-
 routes.post('/superlista', ListaController.create);
 routes.get('/superlista', ListaController.read);
 routes.delete('/superlista/:id', ListaController.delete);
 
 // Rota prioridade
 routes.get('/priorities', PriorityController.read);
-routes.post('/priorities', PriorityController.update);
+routes.post('/priorities/:id', PriorityController.update);
+
+// Rota content
+routes.post('/contents/:id', ContentController.update);
 
 module.exports = routes;
